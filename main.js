d1586d80100fc2b488747b2ff0f4e2c524eb746c
Msaeed144 <mohammadsaeed76@gmail.com>
2022-09-09 02:54:25 +0430

    show number on displaybox

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
+
+
+
 let displayBox = document.querySelector(".display")
-let list = document.querySelectorAll(".show-display")
+let list = document.querySelectorAll('.show-display')
 function showDisplay (event) {
-   const x = event.target.innerText
-   if (displayBox.innerHTML == 0) {
-      return displayBox.innerHTML = x;
+   const x = event.target.innerText 
+   if (displayBox.innerHTML == 0){
+      return displayBox.innerHTML = x
    }
-   return displayBox.innerHTML += x;
+   return displayBox.innerHTML += x
 }
-list.forEach(item => {
+list.forEach (item => {
    item.addEventListener("click" , showDisplay)
 })

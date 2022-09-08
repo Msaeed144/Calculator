16ee7b2e22a0445eaff8725fe1baecbe16c002e2
Msaeed144 <mohammadsaeed76@gmail.com>
2022-09-09 03:25:18 +0430

    clear & clear all function create

--- main.js
+++ main.js
@@ -15,6 +15,19 @@ function calculate() {
    displayBox.innerText = eval(result)
 }
 document.querySelector(".calculate").addEventListener("click" , calculate)
+document.querySelector(".all-clear").addEventListener("click",allClear)
+document.querySelector(".clear-last").addEventListener("click" , clear)
 list.forEach (item => {
    item.addEventListener("click" , showDisplay)
 })
+function allClear (){
+   displayBox.innerText = 0
+}
+function clear (){
+   let text = displayBox.innerText;
+   if (text.length === 1) {
+      displayBox.innerText = 0
+   }else {
+      displayBox.innerText = text.substring(0 , text.length -1)
+   }
+}
